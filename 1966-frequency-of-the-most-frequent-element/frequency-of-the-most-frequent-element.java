f83ff402181210e0d3855c61c42ac10eac0c299c
Nandhika Jhansi <91628798+Nandhika2003@users.noreply.github.com>
2024-07-08 03:46:49 +0530

    Time: 30 ms (47.05%) | Memory: 56.4 MB (66.62%) - LeetSync

--- 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
+++ 1966-frequency-of-the-most-frequent-element/frequency-of-the-most-frequent-element.java
@@ -2,20 +2,19 @@ class Solution {
     public int maxFrequency(int[] nums, int k) {
         Arrays.sort(nums);
         int left = 0, right = 0;
-        long res = 0, total = 0;
+        long total = 0, res = 0;
 
-        while (right < nums.length) {
+        while ( right < nums.length) {
             total += nums[right];
 
-            while (nums[right] * (right - left + 1L) > total + k) {
+            while(nums[right]*(right-left+1l) > total + k) {
                 total -= nums[left];
-                left += 1;
+                left++;
             }
 
-            res = Math.max(res, right - left + 1L);
-            right += 1;
+            res = Math.max(res,right-left+1l);
+            right++;   
         }
-
-        return (int) res;        
+        return (int)res;
     }
 }
